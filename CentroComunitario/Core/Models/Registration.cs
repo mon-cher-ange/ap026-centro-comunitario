f101d90762e9db52a0509bd1a901f5622acfe950
Angel Gonzalo Gutierrez <chromaticsadboy@gmail.com>
2025-09-15 20:04:21 -0300

    Set up project structure

---
+++ CentroComunitario/Core/Models/Registration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core
+{
+    public class Registration
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private DateTime _date;
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+        private int _courseId;
+
+        public int CourseId
+        {
+            get { return _courseId; }
+            set { _courseId = value; }
+        }
+        private int _studentId;
+
+        public int StudentId
+        {
+            get { return _studentId; }
+            set { _studentId = value; }
+        }
+        private int _stateId;
+
+        public int StateId
+        {
+            get { return _stateId; }
+            set { _stateId = value; }
+        }
+    }
+}
---
+++ CentroComunitario/Core/Models/Role.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core
+{
+    public class Role
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+    }
+}
